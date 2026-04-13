8cfb0f657b44300b58e6675c3b2965d6f37683eb
RefiloeM123 <st10513964@rcconnect.edu.za>
2026-04-13 12:39:37 +0200

    fhyjui

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Registration  {
+
+    Scanner scanner = new Scanner(System.in);
+
+    String userName;
+    String password;
+    String phoneNumber;
+    String FirstName;
+    String LastName;
+
+    public boolean checkuserName (String username) {
+       return username.length() <=5 && username.contains("_");
+        {
+
+     public boolean checkPassword (String password )  {
+         if
+
+
+
+
